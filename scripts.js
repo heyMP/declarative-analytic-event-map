e3dd5af14ff1fd1968c5af168aae664708d82810
Michael Potter <mgp140@gmail.com>
2023-06-12 17:24:11 -0400

    refactor

--- scripts.js
+++ scripts.js
@@ -5,53 +5,94 @@ document.addEventListener('overlay-change', eddlEventHandler)
 let messageQueue = new Map();
 
 function eddlEventHandler(e) {
-  const path = e.composedPath();
-  const [eventSource, eventMap] = e.target?.closest('[data-analytics-event-map]')?.getAttribute('data-analytics-event-map')?.split(':') ?? [];
-  // let's remap this event as a new type of event
-  if (eventSource) {
+  // if there is already an event of this type in the queue then we'll debounce it.
+  if (messageQueue.has(e.type)) return;
+
+  const attributes = extractAnalyticAttributesFromEvent(e);
+
+  // let's re-map this event as a new type of event
+  if (attributes.has('data-analytics-event-map')) {
+    const [eventSource, eventMap] = attributes.get('data-analytics-event-map').split(':') ?? [];
     if (e.type === eventSource) {
-      messageQueue.set(eventMap, e);
+      messageQueue.set(eventMap, { type: e.type, attributes });
     }
     else {
       if (!messageQueue.has(e.type)) {
-        messageQueue.set(eventMap, e);
+        messageQueue.set(e.type, { type: e.type, attributes });
       }
     }
   }
   else {
     if (!messageQueue.has(e.type)) {
-      messageQueue.set(eventMap, e);
+      messageQueue.set(e.type, { type: e.type, attributes });
     }
   }
 
   requestAnimationFrame(() => {
     if (messageQueue.size > 0) {
       // pull current queue
-      const queue = messageQueue.values();
+      const queue = messageQueue.entries();
       // create a fresh queue
       messageQueue = new Map();
-      for (let event of queue) {
-        console.log(extractAnalyticsFromEvent(event));
+      for (let [_, item] of queue) {
+        log(item);
       }
     }
   });
 }
 
-function extractAnalyticsFromEvent(event) {
-  const region = event.closest('[data-analytics-region]')?.getAttribute('data-analytics-region');
-  const text = event.closest('[data-analytics-text]')?.getAttribute('data-analytics-text');
-  const category = event.closest('[data-analytics-category]')?.getAttribute('data-analytics-category');
-  const dataMap = extractDataMapFromEvent(event);
-  return { region, text, category, ...dataMap }
+/**
+  * Takes an event listener event and traveres the event path
+  * to collect all of the data-analytic attributes, starting at
+  * the root of the document and ending at the event target.
+  *
+  * If there are duplicate attributes then the attributes closest
+  * to the event target wins.
+  */
+function extractAnalyticAttributesFromEvent(event) {
+  const attributes = new Map();
+  for (const item of event.composedPath().reverse()) {
+    if (!!item.hasAttributes?.()) {
+      for (const attr of item.attributes) {
+        if (attr.name.startsWith('data-analytics')) {
+          attributes.set(attr.name, attr.value);
+        }
+        // if this item has an attribute for data-map then
+        // grab the data that was specified in the attributes
+        // and add it to the map
+        if (attr.name === 'data-analytics-data-map') {
+          const [eventType, dataNames] = attr.value.split(':') ?? [];
+          // ensure that the eventType declared on the attributes
+          // is this event type
+          if (eventType === event.type) {
+            // the name of the data properties are specified as a
+            // comma separated list
+            for (let name of dataNames.split(',')) {
+              attributes.set(name, event[name]);
+            }
+          }
+        }
+      }
+    }
+  }
+  return attributes;
 }
 
-function extractDataMapFromEvent(event) {
-  const dataMap = event.closest('[data-analytics-category]')?.getAttribute('data-analytics-category');
+function replacer(key, value) {
+  if(value instanceof Map) {
+    return {
+      dataType: 'Map',
+      value: Array.from(value.entries()), // or with spread: value: [...value]
+    };
+  } else {
+    return value;
+  }
 }
 
-
-
-
-
-
-
+function log(output) {
+  console.log(output);
+  const pre = document.querySelector('#log pre');
+  pre.insertAdjacentHTML('afterbegin', `
+${JSON.stringify(output, replacer, ' ')}
+`)
+}
